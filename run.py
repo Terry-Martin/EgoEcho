e647fb8d8e0180d0fbf38c0a39db897546c91f98
Terry Martin <terry.martin@gmail.com>
2024-07-24 20:26:35 +0000

    Display first question on test from google sheet

--- run.py
+++ run.py
@@ -12,8 +12,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ego_echo')
 
-test = SHEET.worksheet('test_sheet')
+psychopathy_sheet = SHEET.worksheet('psychopathy')
 
-data = test.get_all_values()
 
-print(data)
+
+print("Instructions")
+print("This quiz is designed to help give you some idea about whether or not you may be a psychopath or sociopath, or have psychopathic tendencies. This quiz is not meant to diagnose psychopathy or tell you definitively whether or not you’re a psychopath. But it will give you a pretty good idea, based upon the research. For each item, indicate how much you agree or disagree with the statement. Take your time and answer truthfully for the most accurate results.")
+data = psychopathy_sheet.get_all_values()
+question = str(data[1])
+print(question)
+
