bf616a49c9c23a3ea79b75fa2d8a918a933d69c7
Terry Martin <terry.martin@gmail.com>
2024-07-26 10:18:19 +0000

    Fix overall_score sum issue by changing scope of variable

--- run.py
+++ run.py
@@ -14,6 +14,10 @@ SHEET = GSPREAD_CLIENT.open('ego_echo')
 
 psychopathy_sheet = SHEET.worksheet('psychopathy')
 
+
+"""
+Function details
+"""
 def display_test_questions():
     data = psychopathy_sheet.get_all_values()
 
@@ -21,17 +25,19 @@ def display_test_questions():
     print("This quiz is designed to help give you some idea about whether or not you may be a psychopath or sociopath, or have psychopathic tendencies. This quiz is not meant to diagnose psychopathy or tell you definitively whether or not you’re a psychopath. But it will give you a pretty good idea, based upon the research. For each item, indicate how much you agree or disagree with the statement. Take your time and answer truthfully for the most accurate results.")
 
     question_count = 1
+    overall_score = 0
+
     while question_count < 13:
         question = data[question_count][0]
         print(f"\nQuestion {question_count}: {question}\n")
         question_count = question_count + 1
 
-        answer = data[0][1]
-        print(f"1: {answer}")
-        answer = data[1][1]
-        print(f"2: {answer}")
-        answer = data[2][1]
-        print(f"3: {answer}\n")
+        option_one = data[0][1]
+        print(f"1: {option_one}")
+        option_two = data[1][1]
+        print(f"2: {option_two}")
+        option_three = data[2][1]
+        print(f"3: {option_three}\n")
 
         user_answer = input("Please select 1, 2 or 3: \n")
         print(f"You have chosen: {user_answer}")
@@ -46,7 +52,7 @@ def display_test_questions():
             print("Input Error")
         print(f"Your score this question is {score_this_question}\n")
 
-        overall_score = 0
+        
         overall_score = int(overall_score) + int(score_this_question)
         print(f"Your overall score so far is {overall_score}\n")
 
