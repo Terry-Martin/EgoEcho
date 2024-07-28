74d0c27bfc6f5b7af701ef06e82710eb52875e32
Terry Martin <terry.martin@gmail.com>
2024-07-28 21:46:01 +0000

    Create display_emotional_intelligence_questions. Read questions from emotional intelligence google sheet. Display questions to user

--- run.py
+++ run.py
@@ -12,13 +12,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ego_echo')
 
-psychopathy_sheet = SHEET.worksheet('psychopathy')
+
 
 
 """
 Function details
 """
 def display_test_questions():
+
+    psychopathy_sheet = SHEET.worksheet('psychopathy')
+
     data = psychopathy_sheet.get_all_values()
 
     print("Instructions")
@@ -68,16 +71,57 @@ def user_result(data):
 
     elif data < 18:
         print("Psychopathy possible\n")
-        print("You answered this quiz consistent with people who have moderately elevated scores on measures of psychopathy and psychopathic behavior. This may suggest a tendency for some psychopathic behaviors, especially when such behaviors result in your ")
+        print("You answered this quiz consistent with people who have moderately elevated scores on measures of psychopathy and psychopathic behavior. This may suggest a tendency for some psychopathic behaviors, especially when such behaviors result in your personal gain.")
 
     else :
-        print("Psychopathy Likely")
+        print("Psychopathy Likely\n")
         print("You answered this quiz consistent with people who score high on measures of psychopathy and psychopathic behavior. This high score suggests that you likely have psychopathic tendencies.")
 
 
+"""
+Function details
+"""
+def display_emotional_intelligence_questions():
+    emotional_intelligence_sheet = SHEET.worksheet('emotional_intelligence')
+    data = emotional_intelligence_sheet.get_all_values()
+
+    question_count = 1
+    overall_score = 0
+
+    while question_count < 21:
+        question = data[question_count][0]
+        print(f"\nQuestion {question_count}: {question}\n")
+        question_count = question_count + 1
+
+        option_one = data[0][1]
+        print(f"1: {option_one}")
+        option_two = data[1][1]
+        print(f"2: {option_two}")
+        option_three = data[2][1]
+        print(f"3: {option_three}\n")
+
+        user_answer = input("Please select 1, 2 or 3: \n")
+        print(f"You have chosen: {user_answer}")
+
+        if int(user_answer) == 1:
+            score_this_question = 0
+        elif int(user_answer) == 2:
+            score_this_question = 1
+        elif int(user_answer) == 3:
+            score_this_question = 2
+        else :
+            print("Input Error")
+        print(f"Your score this question is {score_this_question}\n")
+
+        
+        overall_score = int(overall_score) + int(score_this_question)
+        print(f"Your overall score so far is {overall_score}\n")
+
+
 def main():
-    user_grade = display_test_questions()
-    user_result(user_grade)
+    #user_grade = display_test_questions()
+    #user_result(user_grade)
+    display_emotional_intelligence_questions()
 
 
 main()
